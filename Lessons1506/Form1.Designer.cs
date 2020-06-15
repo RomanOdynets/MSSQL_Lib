8a7126adf29c61c7c08b3859ef91db1707a24db8
Roman Odynets <r.chernenko28@gmail.com>
2020-06-15 19:50:42 +0300

    Created

--- Lessons1506/Form1.Designer.cs
+++ Lessons1506/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.login_tb = new System.Windows.Forms.TextBox();
+            this.pass_tb = new System.Windows.Forms.TextBox();
+            this.auth_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // login_tb
+            // 
+            this.login_tb.Location = new System.Drawing.Point(328, 113);
+            this.login_tb.Name = "login_tb";
+            this.login_tb.Size = new System.Drawing.Size(100, 20);
+            this.login_tb.TabIndex = 0;
+            // 
+            // pass_tb
+            // 
+            this.pass_tb.Location = new System.Drawing.Point(328, 161);
+            this.pass_tb.Name = "pass_tb";
+            this.pass_tb.Size = new System.Drawing.Size(100, 20);
+            this.pass_tb.TabIndex = 1;
+            // 
+            // auth_btn
+            // 
+            this.auth_btn.Location = new System.Drawing.Point(328, 199);
+            this.auth_btn.Name = "auth_btn";
+            this.auth_btn.Size = new System.Drawing.Size(100, 23);
+            this.auth_btn.TabIndex = 2;
+            this.auth_btn.Text = "Try";
+            this.auth_btn.UseVisualStyleBackColor = true;
+            this.auth_btn.Click += new System.EventHandler(this.auth_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.auth_btn);
+            this.Controls.Add(this.pass_tb);
+            this.Controls.Add(this.login_tb);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox login_tb;
+        private System.Windows.Forms.TextBox pass_tb;
+        private System.Windows.Forms.Button auth_btn;
     }
 }
 
--- Lessons1506/Form1.cs
+++ Lessons1506/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,32 @@ namespace Lessons1506
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void auth_btn_Click(object sender, EventArgs e)
+        {
+            string l = login_tb.Text;
+            string p = pass_tb.Text;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Program.db.cnn;
+            cmd.CommandText = "SELECT * FROM users WHERE username = @param1 AND password = @param2";
+            cmd.Parameters.AddWithValue("@param1", l);
+            cmd.Parameters.AddWithValue("@param2", p);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if(reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    MessageBox.Show("Welcome, " + reader.GetValue(1));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username not valid");
+            }
+            reader.Close();
         }
     }
 }
